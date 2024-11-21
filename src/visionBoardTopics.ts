7ced7ec237b73474a6a7d18b28df26701ed6a1c4
zol22 <zol2295@live.com>
2024-11-21 12:30:01 -0500

    add health description

--- src/visionBoardTopics.ts
+++ src/visionBoardTopics.ts
@@ -44,7 +44,7 @@ export const topics = [
     {
         src:'https://plus.unsplash.com/premium_photo-1680042813126-d2ad016b28e0?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         title: 'Health',
-        description: 'Description about Health',
+        description: 'Maintain a healthy lifestyle for physical and mental well-being',
         id: 7,
     },
     {
